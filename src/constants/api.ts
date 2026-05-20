801eb20d6eea5291a567094c79989005cbc9a3c9
Pedro Gregori <ferreiragregorimelo@gmail.com>
2026-05-20 09:15:49 -0300

    fix https geocoding url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = "https://api.openweathermap.org/data/2.5";
-export const API_GEO_URL = "http://api.openweathermap.org/geo/1.0";
+export const API_GEO_URL = "https://api.openweathermap.org/geo/1.0";
 export const API_KEY = process.env.EXPO_PUBLIC_WEATHER_API_KEY ?? "";
 export const DEFAULT_PARAMS = {
   units: "metric",
